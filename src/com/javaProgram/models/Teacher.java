498f1541feb94d4c841a084135b0e3be518ca935
Vaibhav Ganjave <98972943+Ganjave97@users.noreply.github.com>
2022-10-18 11:34:59 +0530

    All Files Upload Successfully

---
+++ src/com/javaProgram/models/Teacher.java
@@ -0,0 +1,34 @@
+package com.javaProgram.models;
+
+public class Teacher {
+
+
+	private int id;
+	private String fname;
+	private String lname;
+	private int age;
+	
+	public Teacher(int id, String fname, String lname, int age) {
+		super();
+		this.id = id;
+		this.fname = fname;
+		this.lname = lname;
+		this.age = age;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+}
